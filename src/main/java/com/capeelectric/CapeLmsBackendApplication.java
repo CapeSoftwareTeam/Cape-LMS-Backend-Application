1921dcc41a8f80fe09fbaed8b96d5d00609270f6
Priyanka-CAPE <107191799+Priyanka-CAPE@users.noreply.github.com>
2022-10-14 16:42:52 +0530

    firstcommit

---
+++ src/main/java/com/capeelectric/CapeLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capeelectric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeLmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capeelectric/CapeLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capeelectric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapeLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
